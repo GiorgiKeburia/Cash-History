f1cbc4a80a0fe27cb64ca8c08343b1435b0900bf
giorgikeburia <giorgikeburia1@gmail.com>
2021-10-03 23:32:01 +0400

    e

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Expenses from './components/Expenses/Expenses';
 const App = () => {
 	const expenses = [
 		{
-			id: 'e1',
+			id: 'e11',
 			title: 'Toilet Paper',
 			amount: 94.12,
 			date: new Date(2020, 7, 14),
